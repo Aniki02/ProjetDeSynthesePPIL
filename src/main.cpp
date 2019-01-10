fa4c3aedc219ffbb865ac3cf1d0c206b5b05fbdf
aniki02 <brahim.zitoun@hotmail.fr>
2019-01-10 23:04:57 +0100

    J'ai tester la classe Vecteur2D , j'ai eu des erreur de fou, le prof a fais une faute dans operator << je l'ai corriger . et toute les fonction inline me donner plein d'erreur que je comprend pas . j'ai enlever les inline et ça marche sauf pour la multiplication j'ai encore une erreur que j'arrive pas a corriger il faudra demander au prof . du coup pour l'instant la classe Vecteur2D ne marche pas

---
+++ src/main.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <iostream>
+#include "vecteur.hpp"
+
+using namespace std;
+
+int main()
+{
+	cout << "essai des vecteurs 2D \n";
+
+	Vecteur2D u1(2,3), u2(2,3), v(5), v1(35,-63), u3(3,4), u4(3,-4);
+
+	cout << " u1 = " <<  u1 << endl;
+	cout << " u2 = " <<  u2 << endl;
+	cout << " u1 + u2 = " <<  u1+u2 << endl;
+	cout << " 5*u1 = " <<  5.8*u1 << endl;
+}
--- src/vecteur.hpp
+++ src/vecteur.hpp
@@ -14,16 +14,16 @@ class Vecteur2D
 public:
 double x,y;
 
-inline explicit Vecteur2D(const double & x = 0, const double & y = 0);
+Vecteur2D(const double & x = 0, const double & y = 0);
 
-inline Vecteur2D(const char * s);
-inline const Vecteur2D operator + (const Vecteur2D & u) const;
-inline const Vecteur2D operator * (const double & a) const;
-inline const Vecteur2D operator - () const;
+Vecteur2D(const char * s);
+const Vecteur2D operator + (const Vecteur2D & u) const;
+const Vecteur2D operator * (const double & a) const;
+const Vecteur2D operator - () const;
 
 operator string() const;
 };
 
-extern ostream & operator << (ostream &,double);
+extern ostream & operator << (ostream &,const Vecteur2D &);
 
 #endif
