a013eb0b87dc55b7bc6a80defb2eb436ab00ef85
aniki02 <brahim.zitoun@hotmail.fr>
2019-01-11 12:50:25 +0100

    ajout des 3 methode de transformation qui renvoi un nouvelle forme

--- src/Groupe.hpp
+++ src/Groupe.hpp
@@ -1,26 +1,11 @@
-#ifndef _FORME_HPP
-#define _FORME_HPP
+#ifndef _GROUPE_HPP
+#define _GROUPE_HPP
 
-#include <stdio.h>
-#include <iostream>
-
-using namespace std;
-
-class Vecteur2D;
-class VisitorDessinerForme;
-/**
-Une forme geometrique est caracterise par un couleur 
-un int défini par les constantes static
-*/
-class Forme{
-	int _couleur; 
+#include
+#include <Forme.hpp>
+class Groupe : public Forme{
+	vector<Forme *> _groupes;
 public:
-	static const int BLACK = 1, BLUE = 2, RED = 3, GREEN = 4, YELLOW = 5, CYAN = 6;
-	Forme(int couleur);
-	virtual ~Forme();
-
-	int getCouleur() const ;
-
 	virtual void translation (const Vecteur2D & vecTrans) = 0;
 	virtual void homothetie (const Vecteur2D & pInvariant, const double & rapportHomothetie) = 0;
 	virtual void rotation (const Vecteur2D & pInvariant, double radiant) = 0;
@@ -33,15 +18,6 @@ public:
 	virtual Forme * clone() const = 0;
 	virtual operator string() const = 0;
 
-	virtual void accepteDessin(VisitorDessinerForme * v) const = 0; 
-
+	virtual void accepteDessin(VisitorDessinerForme * v) const = 0;
 };
-
-extern ostream & operator << (ostream & , const Forme &);
-
-
-
-
-
-
 #endif
